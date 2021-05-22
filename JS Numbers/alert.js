724193451160c2b1e845e5262b25d045a9d15bf8
Batuhan <akcay.batu76@gmail.com>
2021-05-22 18:18:45 +0300

    JS Practices

---
+++ JS Numbers/alert.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+let yonetici;
+let isim;
+
+isim = 'Ahmet';
+yonetici = isim;
+
+alert (isim);
+*/
+
+/*
+let gezegenIsmi;
+let anlikKullanici;
+*/
+
+/*
+const DOGUMGUNU = '18.04.1998';
+const YAS = DOGUMGUNU;
+
+alert (YAS);
+*/
