7d98928e44739cf01be6bc62a828c2e2782ba44e
Batuhan <akcay.batu76@gmail.com>
2021-05-24 21:27:47 +0300

    JS Practices

---
+++ JS Practice/course.js
@@ -0,0 +1,42 @@
+console.log("Merhaba Dünya");
+console.log("Batuhan 'Büyük' Akçay");
+console.log("Batuhan Mehmet'e \"Merhaba\" dedi");
+console.log("Batuhan", 22, "Yaşına", 'Giriyor');
+console.log(0 <= 1);
+console.log(0.1 + 0.2 == 0.3);
+console.log("Merhaba " + "Dünya");
+console.log(true && true, true && false, false && false);
+console.log(true || true, true || false, false || false);
+console.log(!true, !false);
+console.log("4" - 1);
+console.log("4" + 1);
+console.log(4 === "4", true == 1, false == 0);
+
+let myLuckyNumber = 9;
+console.log(myLuckyNumber);
+
+myLuckyNumber = 7;
+console.log(myLuckyNumber);
+
+myLuckyNumber = 4 + 5;
+console.log(myLuckyNumber)
+
+const myConstNumber = 8;
+console.log(myConstNumber);
+
+//let myLuckyNumber = 10;
+//newNumber = 10 + myLuckyNumber;
+//console.log(newNumber);
+
+let newNumber = 10;
+
+newNumber += 25;
+console.log(newNumber);
+
+newNumber++;
+console.log(newNumber);
+
+newNumber--;
+console.log(newNumber);
+
+console.log("Merhaba Dünya".length);
