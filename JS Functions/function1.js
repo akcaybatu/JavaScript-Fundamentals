ab04eb06e637a2cf50c67f291afe06a3be9d2e96
Batuhan <akcay.batu76@gmail.com>
2021-05-27 15:04:41 +0300

    JS Practice

---
+++ JS Functions/function1.js
@@ -0,0 +1,17 @@
+function sayHello(){
+    console.log("Hello")
+}
+sayHello();
+
+
+function sayHelloWithName(name){
+    console.log("Hello " + name)
+}
+sayHelloWithName('Mark');
+
+
+function f(x, y){
+    return 2*x + 5*y + 5;
+}
+result = f(1, 2);
+console.log(result);
