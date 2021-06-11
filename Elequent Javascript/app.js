9e217bf564faa5479571b6d2335d167daa955ba6
Batuhan <akcay.batu76@gmail.com>
2021-06-11 19:24:23 +0300

    JS Practice

--- Elequent Javascript/app.js
+++ Elequent Javascript/app.js
@@ -21,3 +21,24 @@ return --> factorial(4) * 5
        --> factorial(1) * 2
        --> factorial(0) * 1
 */
+
+let number = 0;
+while (number <= 12){
+    console.log(number);
+    number = number +2;
+}
+
+let result = 1;
+let counter = 0;
+while (counter < 10){
+    result = result * 2;
+    counter = counter + 1;
+}
+console.log(result);
+
+if (false != true){
+    console.log("There you are")
+    if (1 < 2)
+        console.log("I think you got it")
+}
+
