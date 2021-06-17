5d35cf2eca041b4176c40a41c431b274eb079b42
Batuhan <akcay.batu76@gmail.com>
2021-06-17 13:53:55 +0300

    JS Practice

--- Elequent Javascript/Functions/function-exercises.js
+++ Elequent Javascript/Functions/function-exercises.js
@@ -20,3 +20,26 @@ function isEven(num) {
     }
 }
 console.log(isEven(-50));
+
+//Bean Counting
+function countBs(bean){
+    let n = 0;    
+    for (let a = 0; a < bean.length; a++){
+        if (bean[a] == "B"){
+            n += 1;
+        }
+    }    
+    console.log(n);
+}
+countBs("BaBaBa");
+
+function countChar(beanny, letter){
+    let m = 0;    
+    for (let b = 0; b < beanny.length; b++){
+        if (beanny[b] == `${letter}`){
+            m += 1;
+        }
+    }    
+    console.log(m);
+}
+countChar("BaBaBaaaaaa", "a");
